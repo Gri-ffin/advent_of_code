1c96e8ddeeb823d210f8fd60281f60880ddeaea4
Gri-ffin <gameshunter359@gmail.com>
2024-12-02 19:53:28 +0100

    refactor: rename input file and update report processing logic

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,44 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let mut left_vec = Vec::new();
-    let mut right_vec = Vec::new();
-
     // Open the file
-    let path = Path::new("src/input.txt");
+    let path = Path::new("src/reports.txt");
     let file = File::open(path)?;
     let reader = io::BufReader::new(file);
 
-    // Read the file line by line
-    for line in reader.lines() {
-        let line = line?;
-
-        // Split the lines into words and parse them into numbers
-        let words: Vec<&str> = line.split_whitespace().collect();
-        if let (Ok(left), Ok(right)) = (words[0].parse::<i32>(), words[1].parse::<i32>()) {
-            left_vec.push(left);
-            right_vec.push(right);
-        }
-    }
-
-    // Create a hashmap to keep references
-    let mut right_count = HashMap::new();
-    for &num in &right_vec {
-        *right_count.entry(num).or_insert(0) += 1;
-    }
-
-    let answer: i32 = left_vec
-        .iter()
-        .map(|&num| {
-            if let Some(&count) = right_count.get(&num) {
-                count * num
-            } else {
-                0
-            }
+    // Read the reports to vectors
+    let reports: Vec<Vec<i32>> = reader
+        .lines()
+        .filter_map(Result::ok)
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse::<i32>().expect("Invalid number"))
+                .collect()
         })
-        .sum();
+        .collect();
 
-    println!("the answer is {}", answer);
+    let safe_count = reports.iter().filter(|&report| is_safe(report)).count();
+
+    println!("safe count is {}", safe_count);
 
     Ok(())
+}
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    // report is strictly increasing or strictly decreasing
+    let differences: Vec<i32> = report.windows(2).map(|w| w[1] - w[0]).collect();
+
+    // differences are valid (between -3 and -1 or between 1 and 3)
+    let all_valid = differences
+        .iter()
+        .all(|&diff| (diff >= 1 && diff <= 3) || (diff <= -1 && diff >= -3));
+
+    // report is either strictly increasing or strictly decreasing
+    let all_increasing = differences.iter().all(|&d| d > 0);
+    let all_decreasing = differences.iter().all(|&d| d < 0);
+
+    // Report is only safe if ALL differences are valid AND it's either all increasing or all decreasing
+    all_valid && (all_increasing || all_decreasing)
 }
